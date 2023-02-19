723997d265687e8d2eaeede615d1b1b8b2741577
Clevas87 <cantonaclevas@gmail.com>
2023-02-19 09:38:19 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,20 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-int ch = 'a';
-int CH = 'A';
+{	
+	int ch = 'a';
+	int CH = 'A';
 
-while (ch <= 'z')
-{
-putchar(ch)
-ch++;
-}
-while (CH <= 'Z')
-{
-putchar(CH);
-CH++;
-}
-putchar('\n')
-return (0);
+	while (ch <= 'z')
+	{	
+	putchar(ch)
+	ch++;
+	}	
+	while (CH <= 'Z')
+	{	
+	putchar(CH);
+	CH++;
+	}
+	putchar('\n')
+	return (0);
 }
