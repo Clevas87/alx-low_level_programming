674a5b9f225e4da6bbaaa4b982478a612f00386f
Clevas87 <cantonaclevas@gmail.com>
2023-02-26 03:32:36 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,6 +11,7 @@ int x, y;
 for (x = 0; x <= 10; x++)
 {
 for (y = 0; y <= 14; y++)
+{
 if (y > 9)
 {
 _putchar((y / 10) + '0');
@@ -18,4 +19,5 @@ _putchar((y / 10) + '0');
 _putchar((y % 10) + '0');
 }
 _putchar('\n');
+}
 }
