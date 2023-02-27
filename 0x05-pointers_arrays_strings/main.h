abebd62b282ab088589be3a98decb1f71dc68e0d
Clevas87 <cantonaclevas@gmail.com>
2023-02-27 12:48:43 -0800

    Print rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef main_h
 #define main_h
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,3 +10,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
