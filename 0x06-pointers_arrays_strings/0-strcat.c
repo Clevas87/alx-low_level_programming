f03b9cb266a7822ed3e316ca8e3c6641cf8127c1
Clevas87 <cantonaclevas@gmail.com>
2023-03-01 09:21:05 -0800

    A function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - A function that concatenates two strings
+ * main - check code
  * Return: Always 0
  */
 
 int main(void)
 {
-char s1[98] = "Hello";
+char s1[98] = "Hello ";
 char s2[] = "World\n";
 char *ptr;
 printf("%s\n", s1);
