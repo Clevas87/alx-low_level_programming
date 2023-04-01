098e9eab2e1028a280d2f403ca2700a52767fd49
Clevas87 <cantonaclevas@gmail.com>
2023-03-31 17:53:14 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,95 +1,26 @@
-#include "lists.h"
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _strlen - find string length
- * @str: string
- * Return: length
+ * add_node - A function that adds a new node at
+ * the beginning of a list_t list.
+ * @head: The original linked list.
+ * @str: The string to add to the node.
+ *
+ * Return: The address of the new list or Null if it failed.
  */
-int _strlen(const char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
-}
-
-/**
- * add_node - add node to beginning of linked list
- * @head: linked list
- * @str: data for new node
- * Return: address of new element, or NULL if failed
- */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node; /* create new node */
-
-	if (str == NULL) /* validate input */
-		return (NULL);
-	if (strdup(str) == NULL) /*check if strdup malloc errored */
-		return (NULL);
-
-	new_node = malloc(sizeof(list_t)); /* malloc for new node */
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str); /* set node values */
-	new_node->len = _strlen(str);
-
-	if (head == NULL) /* set what new node points to first */
-		new_node->next = NULL;
-	else
-		new_node->next = *head;
-
-	*head = new_node; /* set head to point to new node */
-
-	return (new_node);
-#include "lists.h"
-
-/**
- * _strlen - find string length
- * @str: string
- * Return: length
- */
-int _strlen(const char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
+list_t *temp;
+int length = 0;
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
+return (NULL);
+while (str[length])
+length++;
+temp->len = length;
+temp->str = strdup(str);
+*head = temp;
+return (temp);
 }
-
-/**
- * add_node - add node to beginning of linked list
- * @head: linked list
- * @str: data for new node
- * Return: address of new element, or NULL if failed
- */
-
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node; /* create new node */
-
-	if (str == NULL) /* validate input */
-		return (NULL);
-	if (strdup(str) == NULL) /*check if strdup malloc errored */
-		return (NULL);
-
-	new_node = malloc(sizeof(list_t)); /* malloc for new node */
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str); /* set node values */
-	new_node->len = _strlen(str);
-
-	if (head == NULL) /* set what new node points to first */
-		new_node->next = NULL;
-	else
-		new_node->next = *head;
-
-	*head = new_node; /* set head to point to new node */
-
-	return (new_node);
-}}
